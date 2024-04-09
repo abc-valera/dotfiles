af6eecef0406eb9a9925b95fc7152d1295bc4a60
abc-valera <zoocity14@gmail.com>
2024-04-09 23:42:58 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# dotfiles
+
+This repo contains all the dotfiles I use.
+
+## Requirement
+
+Ensure the `git` is installed
+
+## Installation
+
+First, clone the repo in the `$HOME` directory and `cd` in the repo
+
+```
+git clone git@github.com:abc-valera/dotfiles.git
+cd dotfiles
+```
+
+Then use run `./install.sh` to install the dotfiles
---
+++ install.sh
@@ -0,0 +1,59 @@
+#!/usr/bin/bash
+
+# This is an installation script for dotfiles.
+# It initializes the dotfiles with corresponding packages and tools.
+#
+# It supports Fedora (dnf) and Debian (apt) package managers.
+#
+# This script can be used after cloning the dotfiles repository in the $HOME directory.
+
+echo "Installing Dotfiles... 🚀"
+
+# Catch the error and exit
+catch() {
+    echo "Error occurred 🚩"
+    echo "Exiting... ❌"
+    exit 1
+}
+trap catch ERR
+
+# Check if the script is running on a Linux machine
+if [ "$(uname)" != "Linux" ]; then
+    echo "This script is intended for Linux systems only 🚩"
+    exit 1
+fi
+
+# Check if the script was executed from the dotfiles directory
+if [[ ${PWD} != *"/home/"*"/dotfiles" ]]; then
+    echo "This script should be executed from the ~/dotfiles directory 🚩"
+    exit 1
+fi
+
+# Check if the script is runninng in Fedora or Debian
+if [[ -f /etc/redhat-release ]]; then
+    echo "Fedora detected 🤠"
+    sudo dnf update && sudo dnf install -y \
+        stow \
+        fish >/dev/null
+elif [[ -f /etc/debian_version ]]; then
+    echo "Debian detected 🍥"
+    export DEBIAN_FRONTEND=noninteractive &&
+        sudo apt-get update >/dev/null &&
+        sudo apt-get -y install --no-install-recommends \
+            stow \
+            fish >/dev/null
+else
+    echo "This script is intended for Fedora or Debian systems only 🚩"
+    exit 1
+fi
+
+# Install starship prompt
+curl -sS https://starship.rs/install.sh | sudo sh -s -- --yes >/dev/null
+
+# Use stow to create symlinks for all files from the directory
+# (if the file already existed it overrides corresponding file from the dotfiles directory,
+# so they should be restored next)
+sudo stow --adopt .
+sudo git restore .
+
+echo "Dotfiles installed! 🎉"
