3ace39d7ed84db7588ec9d1fee38bac864653134
Valeriy <98162870+abc-valera@users.noreply.github.com>
2025-09-28 15:28:13 +0300

    Change fish theme from 'Snow Day' to 'Tomorrow'

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -5,6 +5,6 @@ status is-interactive || exit
 
 set fish_greeting # Disable greeting
 
-fish_config theme choose 'Snow Day'
+fish_config theme choose Tomorrow
 
 starship init fish | source # Enable starship prompt
