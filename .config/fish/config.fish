a685fc4ca32db67086786ee5f5d6e5a592d2d811
abc-valera <valeriy.tymofieiev@gmail.com>
2026-05-12 21:37:24 +0300

    Enabled emacs keybindings

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -5,6 +5,8 @@ status is-interactive || exit
 
 set fish_greeting # Disable greeting
 
+fish_default_key_bindings # Set default emacs key bindings
+
 fish_config theme choose Tomorrow
 
 starship init fish | source # Enable starship prompt
